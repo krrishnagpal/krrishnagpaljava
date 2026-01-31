6de61c8a42599444a455d7b09fd766a7728415fb
krrishnagpal <palashnagpal0001@gmail.com>
2026-01-31 12:30:33 +0530

    Add files via upload

---
+++ CmdArray.java
@@ -0,0 +1,13 @@
+
+public class CmdArray {
+    public static void main(String args[]) {
+
+        System.out.println("Array elements:");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.print(args[i] + " ");
+        }
+    }
+}
+
+
