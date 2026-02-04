c27fa2e45adcc0c72cad3976d948f469f5e1579f
krrishnagpal <palashnagpal0001@gmail.com>
2026-02-04 08:43:19 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+  
+    Demo() {
+        System.out.println("Constructor called");
+    }
+}
+
+class MainClass {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+    }
+}
+
